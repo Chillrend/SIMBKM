283c57cc1493e2afc72d6b0aa793a8e0651ffe82
Lanang0809 <lanangpras0809@gmail.com>
2023-10-16 10:54:01 +0700

    Add Edit and Delete in kelola-akun

--- resources/views/dashboard/kelola-akun.blade.php
+++ resources/views/dashboard/kelola-akun.blade.php
@@ -60,6 +60,15 @@
                                 <td class="align-middle text-center text-sm">
                                     <span class="badge badge-sm bg-gradient-success">{{ $user->status }}</span>
                                 </td>
+                                <td class="align-middle text-center text-sm">
+                                    <a href="/dashboard/register/kelola-akun/{{ $data->id }}"><span class="badge badge-primary"></span><i class="fa fa-regular fa-pen" style="color: #fecb3e;"></i></a>
+                                    <form action="/dashboard/register/kelola-akun/{{ $data->id }}/delete" method="post" class="d-inline">
+                                        @csrf
+                                    <button class="border-0 bg-transparent" onclick="return confirm('Data Akun Pengguna yang bersangkutan akan ikut terhapus secara permanen, Apakah kamu yakin?')">
+                                        <span class="badge badge-danger"></span>
+                                        <i class="fa fa-solid fa-trash" style="color: #bf0040;"></i>
+                                      </button>
+                                </td>
                             </tr>                        
                             @endforeach
                       </tbody>
