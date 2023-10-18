e6227a2c8d466e233cfb63d408bcc62df0c41106
BrilyanSuherman <brilyansuherman@gmail.com>
2023-10-18 15:05:51 +0700

    Program MBKM
    - dashboard generate nama program + logo + deskripsi

--- resources/views/landing_page/home.blade.php
+++ resources/views/landing_page/home.blade.php
@@ -4,42 +4,48 @@
 
 <main>
 
-    <section class="py-5 text-center container">
-      <div class="row py-lg-5">
-        <div class="col-lg-6 col-md-8 mx-auto">
-          <h1 class="fw-light">Sistem Informasi MBKM</h1>
-          <p class="lead text-body-secondary">Selamat Datang di Sistem Informasi MBKM Politeknik Negeri Jakarta</p>
-          <p>
-            {{-- <a href="#" class="btn btn-primary my-2">Main call to action</a> --}}
-            <a href="#" class="btn btn-secondary my-2">selengkapnya</a>
-            {{-- <button type="button" class="btn btn-secondary btn-lg btn-block">Block level button</button> --}}
-          </p>
-        </div>
+  <section class="py-5 text-center container">
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">Sistem Informasi MBKM</h1>
+        <p class="lead text-body-secondary">Selamat Datang di Sistem Informasi MBKM Politeknik Negeri Jakarta</p>
+        <p>
+          {{-- <a href="#" class="btn btn-primary my-2">Main call to action</a> --}}
+          <a href="#" class="btn btn-secondary my-2">selengkapnya</a>
+          {{-- <button type="button" class="btn btn-secondary btn-lg btn-block">Block level button</button> --}}
+        </p>
       </div>
-    </section>
-  
-    <div class="album py-5 bg-body-tertiary">
-      <div class="container">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    </div>
+  </section>
 
-          <div class="col-md-4">
-            <div class="card h-100 shadow-sm" >
-                <img src="img/kampus_mengajar.JPG" width="100" height="200" class="card-img-top" alt="kampus_mengajar_logo">
-              <div class="card-body">
-                <h3 class="card-title">Kampus Mengajar</h3>
-                <p class="card-text ">Kampus Mengajar merupakan kanal pembelajaran yang memberikan kesempatan kepada mahasiswa untuk belajar di luar kampus selama satu semester guna melatih kemampuan menyelesaikan permasalahan yang kompleks dengan menjadi mitra guru untuk berinovasi dalam pembelajaran, pengembangan strategi, dan model pembelajaran yang kreatif, inovatif, dan menyenangkan.</p>
+
+  <div class="album py-5 bg-body-tertiary">
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+        @foreach($programMbkm as $program)
+        <div class="col-md-4">
+          <div class="card h-100 shadow-sm">
+            <img src="{{ asset('storage/' . $program['fotoikon']) }}" width="300" height="220" alt="gambar">
+
+            <!-- <img src="gambar_ikon/{{ $program->gambar_ikon }}" width="100" height="200" class="card-img-top" alt="gambar"> -->
+            <div class="card-body">
+              <h3 class="card-title">{{ $program->name }}</h3>
+              <p class="card-text ">{{ $program->deskripsi }}</p>
+            </div>
+            <div class="d-flex justify-content-between align-items-end p-3">
+              <!-- <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                {{-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> --}}
               </div>
-              <div class="d-flex justify-content-between align-items-end p-3">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  {{-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> --}}
-                </div>
-                <small class="text-body-secondary">9 mins</small>
-              </div>
+              <small class="text-body-secondary">9 mins</small> -->
             </div>
           </div>
+        </div>
+        @endforeach
 
-          <div class="col-md-4">
+
+        <!-- <div class="col-md-4">
             <div class="card h-100 shadow-sm">
                 <img src="img/msib.JPG" width="100" height="200"  class="card-img-top" alt="kampus_mengajar_logo">
               <div class="card-body">
@@ -179,10 +185,10 @@
                 <small class="text-body-secondary">9 mins</small>
               </div>
             </div>
-          </div>
-        </div>
+          </div> -->
       </div>
     </div>
-  
-  </main>
+  </div>
+
+</main>
 @endsection
