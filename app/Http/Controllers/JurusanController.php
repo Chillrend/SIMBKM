feb1e3a7ef372dcb3bd557904c1d4783c7acfa6e
Rinaldito <rinalditoryanari@gmail.com>
2024-01-23 14:46:54 +0700

    Connecting Table Prodi to API Prodi

--- app/Http/Controllers/JurusanController.php
+++ app/Http/Controllers/JurusanController.php
@@ -2,55 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ApiHelper;
 use Illuminate\Http\Request;
 use App\Models\Jurusan;
 use App\Models\Fakultas;
 
 class JurusanController extends Controller
 {
-    public function index(){
-        return view('dashboard.jurusan',[
-            'title' => 'Prodi',
+    public function index()
+    {
+        $client = new ApiHelper(config('app.api_url'), config('app.api_user'), config('app.api_password'));
+
+        $prodis = $client->get('/prodi/all')->getBody()->getContents();
+        $prodis = collect(json_decode($prodis));
+
+        $prodis = $prodis->map(function ($prodi) use ($client) {
+            $array = (object)($prodi);
+
+            if ($prodi->id_jurusan) {
+                $jurusan      = $client->get('/jurusan/find/' . $prodi->id_jurusan)->getBody()->getContents();
+                $jurusan      = (object)json_decode($jurusan);
+                $nama_jurusan = $jurusan->nama_jurusan;
+            } else {
+                $nama_jurusan = '-';
+            }
+            $array->nama_jurusan = $nama_jurusan;
+
+            return $array;
+        });
+
+        return view('dashboard.jurusan', [
+            'title'      => 'Prodi',
             'title_page' => 'Prodi',
-            'active' => 'Jurusan',
-            'name' => auth()->user()->name,
-            'jurusan' => Jurusan::all()
+            'active'     => 'Jurusan',
+            'name'       => auth()->user()->name,
+            'jurusan'    => $prodis,
         ]);
     }
 
-    public function create(){
+    public function create()
+    {
         return view('dashboard.create-jurusan', [
-            'title' => 'Create',
+            'title'      => 'Create',
             'title_page' => 'Prodi / Create',
-            'active' => 'Jurusan',
-            'name' => auth()->user()->name,
-            'fakultas' => Fakultas::all()
+            'active'     => 'Jurusan',
+            'name'       => auth()->user()->name,
+            'fakultas'   => Fakultas::all()
         ]);
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $validatedData = $request->validate([
-            'name' => 'required|unique:jurusan',
+            'name'        => 'required|unique:jurusan',
             'fakultas_id' => 'required',
-            'status' => 'required'
+            'status'      => 'required'
         ]);
         Jurusan::create($validatedData);
 
         return redirect('/dashboard/jurusan')->with('success', 'Prodi Berhasil Dibuat!');
     }
 
-    public function edit($id){
-        return view('dashboard.edit-jurusan',[
-            'title' => 'Edit',
+    public function edit($id)
+    {
+        return view('dashboard.edit-jurusan', [
+            'title'      => 'Edit',
             'title_page' => 'Prodi / Edit',
-            'active' => 'Jurusan',
-            'name' => auth()->user()->name,
-            'jurusan' => Jurusan::find($id),
-            'fakultas' => Fakultas::all()
+            'active'     => 'Jurusan',
+            'name'       => auth()->user()->name,
+            'jurusan'    => Jurusan::find($id),
+            'fakultas'   => Fakultas::all()
         ]);
     }
 
-    public function update(Request $request, $jurusan){
+    public function update(Request $request, $jurusan)
+    {
 
         $postingan = Jurusan::find($jurusan);
 
@@ -59,7 +85,8 @@ class JurusanController extends Controller
 
     }
 
-    public function destroy($id){
+    public function destroy($id)
+    {
         Jurusan::destroy($id);
         return redirect('/dashboard/jurusan')->with('success', 'Data Prodi Berhasil di Hapus');
     }
--- resources/views/dashboard/jurusan.blade.php
+++ resources/views/dashboard/jurusan.blade.php
@@ -1,74 +1,83 @@
 @extends('layout.dashboard')
 @section('container')
 
-@if(session()->has('success'))
-  <div class="alert alert-success col-lg-8" role="alert">
-    {{ session('success') }}
-  </div>
-@endif
+    @if(session()->has('success'))
+        <div class="alert alert-success col-lg-8" role="alert">
+            {{ session('success') }}
+        </div>
+    @endif
 
-<div class="row">
-    <div class="col-12">
-        <div class="card mb-4">
-            <div class="card-header">
-              <div class="ms-md-auto d-flex">
-                <h4>Data Prodi</h4>
-                <a href="/dashboard/jurusan/create" class="btn btn-primary d-flex ms-md-auto ms-3">Buat Prodi</a>
-            </div>
-            </div>
-
-            <div class="card-body">
-
-                <div class="table-responsive p-0">
-                    <table class="table align-items-center mb-0">
-                      <thead>
-                        <tr>
-                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No.</th>
-                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama</th>
-                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Jurusan</th>
-                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Status</th>
-                          <th class="text-secondary opacity-7"></th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        @foreach($jurusan as $data)
-                        <tr>
-                            <td class="align-middle text-center text-sm">
-                                <p class="text-xs font-weight-bold mb-0">{{ $loop->iteration }}</p>
-                            </td>
-                            <td class="text-sm text-center">
-                                <p class="text-xs font-weight-bold mb-0">{{ $data->name }}</p>
-                            </td>
-                            <td class="align-middle text-center text-sm">
-                                <p class="text-xs font-weight-bold mb-0">{{ $data->listFakultas->name }}</p>
-                            </td>
-                            <td class="align-middle text-center text-sm">
-                                @if($data->status == 'Aktif')
-                                  <span class="badge badge-sm bg-gradient-success">{{ $data->status }}</span>
-                                @else
-                                  <span class="badge badge-sm bg-gradient-danger">{{ $data->status }}</span>
-                                @endif    
-                            </td>
-                            <td class="align-middle text-center text-sm">
-                              <td>
-                                <a href="/dashboard/jurusan/{{ $data->id }}" ><span class="badge badge-primary"></span><i class="fa fa-regular fa-pen" style="color: #fecb3e;"></i></a>
-                                <form action="/dashboard/jurusan/{{ $data->id }}/delete" method="post" class="d-inline">
-                                  @csrf
-                                  <button class="border-0 bg-transparent" onclick="return confirm('Are you sure?')">
-                                    <span class="badge badge-danger"></span>
-                                    <i class="fa fa-solid fa-trash" style="color: #bf0040;"></i>
-                                  </button>
-                                </form>
-                              </td>
-                            </td>
-                        </tr>
-                        @endforeach
-                      </tbody>
-                    </table>
+    <div class="row">
+        <div class="col-12">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <div class="ms-md-auto d-flex">
+                        <h4>Data Prodi</h4>
+                        <a href="/dashboard/jurusan/create" class="btn btn-primary d-flex ms-md-auto ms-3">Buat
+                            Prodi</a>
+                    </div>
                 </div>
 
+                <div class="card-body">
+                    <div class="table-responsive p-0">
+                        <table class="table align-items-center mb-0">
+                            <thead>
+                            <tr>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    No.
+                                </th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    Nama
+                                </th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    Jurusan
+                                </th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                    Status
+                                </th>
+                                <th class="text-secondary opacity-7"></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($jurusan as $data)
+                                <tr>
+                                    <td class="align-middle text-center text-sm">
+                                        <p class="text-xs font-weight-bold mb-0">{{ $loop->iteration }}</p>
+                                    </td>
+                                    <td class="text-sm text-start">
+                                        <p class="text-xs font-weight-bold mb-0">{{ $data->nama_prodi }}</p>
+                                    </td>
+                                    <td class="align-middle text-start text-sm">
+                                        <p class="text-xs font-weight-bold mb-0">{{ $data->nama_jurusan }}</p>
+                                    </td>
+                                    <td class="align-middle text-center text-sm">
+                                        <span
+                                            class="badge badge-sm {{($data->status == 'A') ? "bg-gradient-success" : "bg-gradient-danger"}}">{{ $data->status }}</span>
+                                    </td>
+                                    <td class="align-middle text-center text-sm">
+                                    <td>
+                                        <a href="/dashboard/jurusan/{{ $data->id }}">
+                                            <span class="badge badge-primary"></span>
+                                            <i class="fa fa-regular fa-pen" style="color: #fecb3e;"></i>
+                                        </a>
+                                        <form action="/dashboard/jurusan/{{ $data->id }}/delete" method="post"
+                                              class="d-inline">
+                                            @csrf
+                                            <button class="border-0 bg-transparent"
+                                                    onclick="return confirm('Are you sure?')">
+                                                <span class="badge badge-danger"></span>
+                                                <i class="fa fa-solid fa-trash" style="color: #bf0040;"></i>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
